494918f785ec8341cc74fae52cfc1fb7b7f3107a
StevenWD <yyj_stevenwd@163.com>
2018-08-15 14:24:06 +0800

    two evaluation ways

--- eval.py
+++ eval.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 
+import json
 import numpy as np
 from keras.layers import Input, Embedding, LSTM, Dense, Bidirectional, Add, MaxPooling1D, Concatenate, Dot, Flatten
 from keras.models import Model
@@ -11,6 +12,8 @@ from configparser import ConfigParser
 from keras.optimizers import Adam
 from keras import losses
 from sklearn.utils.class_weight import compute_class_weight
+from preprocess import readData
+from preprocess import readRelation
 
 def ranking_loss(y_true, y_pred):
      return K.maximum(0.0, 0.1 + K.sum(y_pred*y_true,axis=-1))
@@ -18,15 +21,15 @@ def ranking_loss(y_true, y_pred):
 def model_construct():
     # CONFIG
     config = ConfigParser()
-    config.read('/home/stevenwd/HR-LSTM/config.ini')
+    config.read('./config.ini')
 
     question_input = Input(shape=(config.getint('pre', 'question_maximum_length'), ), dtype='int32',name="question_input")
     relation_all_input = Input(shape=(config.getint('pre', 'relation_word_maximum_length'), ), dtype='int32',name="relation_all_input")
     relation_input = Input(shape=(config.getint('pre', 'relation_maximum_length'), ), dtype='int32',name="relation_input")
 
-    question_emd = np.load('/home/stevenwd/HR-LSTM/question_emd_matrix.npy')
-    relation_emd = np.load('/home/stevenwd/HR-LSTM/relation_emd_matrix.npy')
-    relation_all_emd = np.load('/home/stevenwd/HR-LSTM/relation_all_emd_matrix.npy')
+    question_emd = np.load('/home/stevenwd/HR-BiLSTM/question_emd_matrix.npy')
+    relation_emd = np.load('/home/stevenwd/HR-BiLSTM/relation_emd_matrix.npy')
+    relation_all_emd = np.load('/home/stevenwd/HR-BiLSTM/relation_all_emd_matrix.npy')
 
     question_emd = Embedding(question_emd.shape[0],
             config.getint('pre', 'word_emd_length'),
@@ -65,7 +68,8 @@ def model_construct():
 
 if __name__ == '__main__':
     # GPU settings
-    os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+    os.environ["CUDA_VISIBLE_DEVICES"] = "6"
+    neg_num = json.load(open('./neg_number.json', 'r'))
 
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
@@ -73,7 +77,7 @@ if __name__ == '__main__':
 
     K.set_session(sess)
     model = model_construct()
-    model.load_weights('my_model_weights.h5')
+    model.load_weights('./my_model_weights_06_2048.h5')
     print(model.summary())
 
     question_feature = np.load('./test_question_feature.npy')
@@ -81,16 +85,56 @@ if __name__ == '__main__':
     relation_feature = np.load('./test_relation_feature.npy')
     relation_all_feature = np.load('./test_relation_all_feature.npy')
 
-    simi_pos = model.predict([question_feature, relation_feature, relation_all_feature])
+    print('positive data loaded...')
+    simi_pos = model.predict([question_feature, relation_feature, relation_all_feature], batch_size=1024)
 
+    print('positive similarity computed...')
     np.save('test_pre_pos.npy', simi_pos)
 
     relation_feature_neg = np.load('./test_relation_feature_neg.npy')
     relation_all_feature_neg = np.load('./test_relation_all_feature_neg.npy')
 
-    simi_neg = model.predict([question_feature, relation_feature_neg, relation_all_feature_neg])
+    print('negtive data loaded...')
+    simi_neg = model.predict([question_feature, relation_feature_neg, relation_all_feature_neg], batch_size=1024)
 
-    np.save('test_pre_pos.npy', simi_neg)
+    print('negtive similarity computed...')
+    np.save('test_pre_neg.npy', simi_neg)
 
     acc = np.sum(simi_pos>simi_neg) / simi_pos.shape[0]
-    print("accurcy: " + acc)
+    print("relation accurcy: " + str(acc))
+
+    index = 0
+    false_list = list()
+    true_list = list()
+    all_set = set()
+
+    config = ConfigParser()
+    config.read('./config.ini')
+    data = readData(config.get('pre', 'test_filepath'))
+    relation = readRelation(config.get('pre', 'relation_filepath'))
+    for num,neg_index in neg_num:
+        if np.sum(simi_pos[index: index+num]-simi_neg[index: index+num]<0) > 0:
+            false_list.append(neg_index)
+            print (simi_pos[index])
+            print (np.max(simi_neg[index: index+num]))
+            print (len(simi_neg[index: index+num]))
+            print (np.argmax(simi_neg[index: index+num]))
+            print (simi_neg[index: index+num][np.argmax(simi_neg[index: index+num])])
+            print (neg_index)
+            print ("")
+            pass
+        else:
+            true_list.append(neg_index)
+        index += num
+        all_set.add(neg_index)
+    print (max(true_list))
+    true_list = set([i for i in true_list if i in all_set and i not in false_list])
+    print (len(all_set))
+    print (len(true_list))
+    for i in all_set:
+        if i not in true_list:
+            print (i)
+    print (data[0])
+    print (relation[0][1])
+    print (relation[1][1])
+    print('sentence accurcy: '+str(len(true_list)/len(all_set)))
